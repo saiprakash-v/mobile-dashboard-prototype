44331cc2106d5a375527d98a90d9a1980d428de3
V Sai Prakash <prakash@beehyv.com>
2019-12-12 15:02:46 +0530

    navigation changes

--- app/scripts/directives/navigation-menu.directive.js
+++ app/scripts/directives/navigation-menu.directive.js
@@ -1,10 +1,37 @@
 angular.module("mobileDashboard")
     .controller('navigationController', ['$scope', function($scope) {
+        $scope.selectedMenuOption = 'programSummary';
         $scope.$on('open_nav_menu',function(event,data){
             document.getElementById('nav-menu').style.left = '0';
         });
         $scope.closeMenu = function () {
             document.getElementById('nav-menu').style.left = '-300px';
+        };
+        $scope.expandOptions = function (expandedSectionId) {
+            if (document.getElementById(expandedSectionId).style.height === '100%') {
+                document.getElementById(expandedSectionId).style.height = '0';
+            } else {
+                document.getElementById(expandedSectionId).style.height = '100%';
+            }
+            if (expandedSectionId !== 'nutrition') {
+                document.getElementById('nutrition').style.height = '0';
+            }
+            if (expandedSectionId !== 'reach') {
+                document.getElementById('reach').style.height = '0';
+            }
+            if (expandedSectionId !== 'demographics') {
+                document.getElementById('demographics').style.height = '0';
+            }
+            if (expandedSectionId !== 'infrastructure') {
+                document.getElementById('infrastructure').style.height = '0';
+            }
+        };
+        $scope.selectMenuOption = function (optionId) {
+            document.getElementById($scope.selectedMenuOption).style.background = 'none';
+            document.getElementById($scope.selectedMenuOption).style.borderRight = 'none';
+            $scope.selectedMenuOption = optionId;
+            document.getElementById(optionId).style.background = '#363c51';
+            document.getElementById(optionId).style.borderRight = '4px solid #00a8c6';
         }
     }])
     .directive('navigationMenu', function () {
